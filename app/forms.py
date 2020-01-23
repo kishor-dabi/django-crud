d272e12d743997372fd1530e74832680a1df66f3
kishor <kishoredabi@cdnsol.com>
2020-01-23 20:00:47 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from app.models import UserProfileInfo
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+
+        model = User
+        fields = ('username', 'password', 'email')
+        attrs = {'class': 'form-control'}
+
+
+class UserProfileInfoForm(forms.ModelForm):
+
+    class Meta:
+
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic')
+        attrs = {'class': 'form-control'}
---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from app import views
+
+
+urlpatterns = [
+    # url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^user_login/$', views.user_login, name='user_login'),
+]
